cf6a6f8163b61dd3d391dbcb1515f69da6562235
Milton Centeno <david1108n@gmail.com>
2026-05-03 15:09:44 -0500

    format

--- plugin/blink.lua
+++ plugin/blink.lua
@@ -5,13 +5,13 @@ local cmp = require('blink.cmp')
 
 
 cmp.setup({
-        keymap = {
-            preset = "enter",
-            ["<C-k>"] = { "select_prev", "fallback" },
-            ["<C-j>"] = { "select_next", "fallback" },
-        },
-        signature = { enabled = true },
-        completion = { documentation = { auto_show = true, auto_show_delay_ms = 200 } },
-        sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-        fuzzy = { implementation = "rust" },
+    keymap = {
+        preset = "enter",
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+    },
+    signature = { enabled = true },
+    completion = { documentation = { auto_show = true, auto_show_delay_ms = 200 } },
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    fuzzy = { implementation = "rust" },
 })
